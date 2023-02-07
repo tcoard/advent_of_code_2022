0f5100bd15f76b6372e8a08b8b6dd0bfde7779cd
Thomas Coard <thomas@sequencing.com>
2023-02-07 18:58:03 -0500

    more progress on day 5

--- day5/day5.hs
+++ day5/day5.hs
@@ -1,36 +1,43 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 
 import Data.Char
 import Data.List
 import Data.List.Split
+import Text.Read
+import Data.Maybe(mapMaybe)
 
-seperateCommands :: [[String]] -> ([String], [String])
-seperateCommands [rawBoxes, commands] = (init rawBoxes, commands)
+seperateBoxesCommands :: [[String]] -> ([String], [String])
+seperateBoxesCommands [rawBoxes, commandSentences] = (init rawBoxes, commandSentences)
 
--- parseLines line =  splitOn "\n\n" line
--- parseLines line = map (\rawRange -> map (\rawInt -> read rawInt :: Int) $ splitOn "-" rawRange) $ splitOn "," line
-
-formatData :: [[Int]] -> (Int, Int, Int, Int)
-formatData [[x1, x2], [y1, y2]] = (x1, x2, y1, y2)
-
-isCompleteOverlap :: (Int, Int, Int, Int) -> Bool
-isCompleteOverlap (x1, x2, y1, y2) = (x1 - y1) * (x2 - y2) <= 0
-
-isPartialOverlap :: (Int, Int, Int, Int) -> Bool
-isPartialOverlap (x1, x2, y1, y2) = (x1 - y2) * (x2 - y1) <= 0
-
-aaa :: String -> String
-aaa xs = case drop 3 xs of
-  y : ys -> y : aaa ys
+parseColumnContents :: String -> String
+parseColumnContents xs = case drop 3 xs of
+  y : ys -> y : parseColumnContents ys
   [] -> []
+
+getIntsFromCommandSentences:: String -> [Int]
+getIntsFromCommandSentences = mapMaybe readMaybe . words
+
+seperateCommands :: [Int] -> (Int, Int, Int)
+seperateCommands [amount, from, to] = (amount, from, to)
+
+aaa :: [String] -> (Int, Int, Int) -> [String]
+aaa boxes (amount, from, to) = x
+    where (x,_:ys) = splitAt (from-1) boxes --splitAt amount (boxes!!from)
 
 main :: IO ()
 main =
   do
     rawInput <- readFile "data.txt"
-    let (rawBoxes, commands) = seperateCommands $ splitOn [""] (lines rawInput)
-    let boxes = map (dropWhile isSpace) $ transpose $ map (\x -> aaa (" "++" "++x)) rawBoxes
+    let (rawBoxes, commandSentences) = seperateBoxesCommands $ splitOn [""] (lines rawInput)
+    -- "  " if for padding to get correct spacing for the first entry
+    let boxes = map (dropWhile isSpace) $ transpose $ map (\x -> parseColumnContents ("  "++x)) rawBoxes
+    let commands = map (seperateCommands . getIntsFromCommandSentences) commandSentences
     print $ boxes
+    print $ commands!!1
+    print $ aaa boxes (commands!!1)
+    --print $ (commands!!1)
 
--- print $ sum $ map (fromEnum . isCompleteOverlap . formatData . parseLines) input
--- print $ sum $ map (fromEnum . isPartialOverlap . formatData . parseLines) input
+-- print $ sum $ map (fromEnum . isCompleteOverlap . formatBoxes . parseLines) input
+-- print $ sum $ map (fromEnum . isPartialOverlap . formatBoxes . parseLines) input
